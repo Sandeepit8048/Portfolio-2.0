70e1f4e22cfe5126e6915809894354b190cc4edc
sandeepit8048 <rkysandeepyadav@gmail.com>
2025-07-13 16:16:24 +0530

    add contact, Nav

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import Eduction from './components/Eduction'
 import Home from './components/Home'
 import Skil from './components/Skil'
+import Navbar from './components/Navbar'
+import Contact from './components/Contact'
 import './App.css'
 
 function App() {
@@ -9,9 +11,13 @@ function App() {
 
   return (
     <>
+     <h1>Welcome to My Portfolio</h1>
+     <p>This is a simple portfolio application built with React.</p>
      <Eduction />
      <Home />
      <Skil />
+      <Navbar />
+     <Contact />
     </>
   )
 }

9551416e6c5c7db39190ff5af6f5062032885cd7
sandeepit8048 <rkysandeepyadav@gmail.com>
2025-07-18 15:53:26 +0530

    Styling Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,19 +2,24 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAddressBook } from '@fortawesome/free-solid-svg-icons';
 import Img from '../assets/contactimg.png';
-import Img2 from '../assets/contact.webp';
+import Img2 from '../assets/img2.avif';
 import '../Style/Home.css'; 
 function Contact() {
+  
   return (
     <>
-    <div className="container">
+    <div className="container mt-7 ">
       <div className="registion ">
        <h1 className='flex items-center justify-center text-3xl font-bold mb-4 gap-2 mb-6 '> 
          <FontAwesomeIcon icon={faAddressBook} className="text-blue-600 text-4xl  space-2" />
            Contact<span>Me</span></h1>
-           <div className="registrationform">
+                <p className='text-center text-gray-500 mt-4 text-center'>Feel free to reach out for any queries or collaborations!</p>
+                <p className='text-gray-500 text-center  '>Or you can contact me  email at <a href="rkysandeepyadav@gmail.com" className='text-blue-600 hover:underline '>Email</a></p>
+                 <img src={Img} alt="" />
+           <div className="registrationform  ">
             <form action="" method="post" className='flex flex-row items-center justify-center'>
               <div className="form-group  bouder p-4 ">
+                <h1 className='text-center font-bold text-3xl '>Form</h1>
                 <label htmlFor="name">Name</label>
                <input type="text" id="name" name="name" required  />  
               <label htmlFor="Email">E-mail</label>
@@ -26,12 +31,9 @@ function Contact() {
               <button type="submit" className='btn bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition'>Send Message</button>
               </div>
               <div>
-                <p className='text-center text-gray-500 mt-4'>Feel free to reach out for any queries or collaborations!</p>
               
               <div className='text-center mt-4'>
-                <p className='text-gray-500'>Or you can contact me  email at <a href="rkysandeepyadav@gmail.com" className='text-blue-600 hover:underline'>Email</a></p>
-                 <img src={Img} alt="" />
-                  {/* <img src={Img2} alt="" className='w-full h-full mt-4' /> */}
+                  <img src={Img2} alt="" className='w-full h-full '/>
               </div>
               </div>
              

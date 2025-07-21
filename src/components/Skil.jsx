d510dda4c2f8ba984a8d87aefff21040e5af3d0b
sandeepit8048 <rkysandeepyadav@gmail.com>
2025-07-21 19:19:24 +0530

    responsive

--- src/components/Skil.jsx
+++ src/components/Skil.jsx
@@ -12,7 +12,7 @@ function Skil() {
 
   return (
     <>
-     <div className="box flex flex-col items-center justify-center p-5 mt-7 flex-wrap">
+     <div className="box  flex-col items-center justify-center p-5 mt-7 flex-wrap">
       <h1 className='text-3xl font-bold mb-4'>Skills</h1>
       <p className='text-gray-500 mb-4'>Here are some of the technologies I have worked with:</p>
        <ul className='list-disc list-inside w-[300px] h-[200px] bg-gray-400'>

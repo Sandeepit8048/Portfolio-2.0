6728a880c7eedbb27a5bf4b655f2017890f7778d
sandeepit8048 <rkysandeepyadav@gmail.com>
2025-07-28 16:49:53 +0530

    styling

--- src/components/Skil.jsx
+++ src/components/Skil.jsx
@@ -8,7 +8,7 @@ import DBMS from '../assets/dbms.png';
 import Next from '../assets/nextjs.webp';
 import java from '../assets/java.png';
 import mongodb from '../assets/mongodb.png';
-import SQL from '../assets/sql.png';
+import SQL from '../assets/SQL.png';
 import Postman from '../assets/postman.svg';
 import router from '../assets/router.jpg';
 import CN from '../assets/CN.png';
